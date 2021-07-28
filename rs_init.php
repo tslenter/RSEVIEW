9894d0513408cdaa23d79bc24370549fcac0a191
Tom Slenter <tomslenter@gmail.com>
2021-07-28 23:52:57 +0200

    Update rs_init.php

--- rs_init.php
+++ rs_init.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'vendor/autoload.php';
+require_once '/opt/rseview/vendor/autoload.php';
 use Elasticsearch\ClientBuilder;
 $es = ClientBuilder::create()->setHosts(['127.0.0.1:9200'])->build();
 ?>
