04e842af2585d077aaa4a4263268bd6c62d9131c
root <root@rssyslog001.LAN.LOCAL>
2022-05-31 07:56:05 +0000

    Added option for authentication ...

--- rs_init.php
+++ rs_init.php
@@ -1,5 +1,13 @@
 <?php
+$hosts = [
+    [
+        'host' => '127.0.0.1',
+        'port' => '9200',
+        'user' => '',
+        'pass' => ''
+    ]
+];
 require_once '/opt/RSEVIEW/vendor/autoload.php';
 use Elasticsearch\ClientBuilder;
-$es = ClientBuilder::create()->setHosts(['127.0.0.1:9200'])->build();
+$es = ClientBuilder::create()->setHosts($hosts)->build();
 ?>
